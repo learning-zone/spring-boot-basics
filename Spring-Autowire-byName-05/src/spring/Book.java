05c9966d027979207e7114d5abde207dd926e133
Pradeep Kumar <pradeep.vwa@gmail.com>
2019-03-11 11:31:22 +0530

    Spring-Autowire-byName

---
+++ Spring-Autowire-byName-05/src/spring/Book.java
@@ -0,0 +1,18 @@
+package spring;
+
+public class Book {
+	private String bookname;
+	private int bookprice;
+	public String getBookname() {
+		return bookname;
+	}
+	public void setBookname(String bookname) {
+		this.bookname = bookname;
+	}
+	public int getBookprice() {
+		return bookprice;
+	}
+	public void setBookprice(int bookprice) {
+		this.bookprice = bookprice;
+	}
+}
