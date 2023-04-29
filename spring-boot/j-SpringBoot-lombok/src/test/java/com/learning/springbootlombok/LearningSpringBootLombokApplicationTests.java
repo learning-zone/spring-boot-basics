eb785beabb8f7bd0c512b771d00aaa93284c1ae2
THOMAS <tommypash405@gmail.com>
2023-04-29 15:52:51 +0300

    initial lombok and data jpa

---
+++ spring-boot/j-SpringBoot-lombok/src/test/java/com/learning/springbootlombok/LearningSpringBootLombokApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.springbootlombok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningSpringBootLombokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ spring-boot/k-SpringBoot-DataJpa/src/test/java/com/learning/springbootdatajpa/LearningSpringBootDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.springbootdatajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningSpringBootDataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
